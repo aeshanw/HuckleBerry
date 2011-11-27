bdd322dd7f328eafe2d8fb6f7e65913e52e856bd
Dhawal Shah <dhawaljshah@gmail.com>
2011-11-27 12:02:18 +0800

    Spree Initial Setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hucklebury::Application.config.secret_token = '798f296f21630e6782bdc169906acc4f57bb11688a30dbc76727780a7a8d1a9b0f1f896f105e0a21f0a47d8a381f99a5254bc75dc63ef5029fa5d9f710a5e1d3'
